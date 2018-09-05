5ebfc199192b37d0895892f1eff95d080f16c887
Rashed Sarwar <rashed.sarwar@ukaea.uk>
2018-09-06 00:28:42 +0100

    Cleaned up unsued codes

--- toolchain-m32.cmake
+++ toolchain-m32.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_SYSTEM_VERSION 1)
 # Define the cross compiler locations
 SET(CMAKE_C_COMPILER   /usr/bin/gcc)
 SET(CMAKE_CXX_COMPILER /usr/bin/g++)
-# Define the sysroot path for the RaspberryPi distribution in our tools folder 
+# Define the sysroot path for the 32-bit distribution in our tools folder 
 SET(CMAKE_FIND_ROOT_PATH /usr/i686-linux-gnu)
 # Use our definitions for compiler tools
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -14,8 +14,5 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 #add_definitions(-funsafe-math-optimizations -ffast-math -D_ARM -Wa,-mimplicit-it=thumb)
 
 set(tm32 TRUE)
-#set(CMAKE_CXX_COMPILER "-std-c11")
 set(CMAKE_SYSTEM_PROCESSOR "i686")
-set(CURSES_LIBRARY /home/rsarwar/rkspace/ncurses-5.9/lib) 
-set(CURSES_INCLUDE_PATH /Home/rsarwar/workspace/ncurses-5.9/lib)
 
