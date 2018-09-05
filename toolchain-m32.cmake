863f15959117d2526c2a2fb2145ee3d10bef0487
Rashed Sarwar <rashed.sarwar@ukaea.uk>
2018-09-05 23:56:06 +0100

    Support for i686-linux cross-compilation from x86_64 linux

---
+++ toolchain-m32.cmake
@@ -0,0 +1,21 @@
+# Define our host system
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+# Define the cross compiler locations
+SET(CMAKE_C_COMPILER   /usr/bin/gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/g++)
+# Define the sysroot path for the RaspberryPi distribution in our tools folder 
+SET(CMAKE_FIND_ROOT_PATH /usr/i686-linux-gnu)
+# Use our definitions for compiler tools
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# Search for libraries and headers in the target directories only
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+#add_definitions(-funsafe-math-optimizations -ffast-math -D_ARM -Wa,-mimplicit-it=thumb)
+
+set(tm32 TRUE)
+#set(CMAKE_CXX_COMPILER "-std-c11")
+set(CMAKE_SYSTEM_PROCESSOR "i686")
+set(CURSES_LIBRARY /home/rsarwar/rkspace/ncurses-5.9/lib) 
+set(CURSES_INCLUDE_PATH /Home/rsarwar/workspace/ncurses-5.9/lib)
+
